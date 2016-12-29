5efd9211830db3fb9385a6082f84e6e3c569472a
杨政伦 <yangzhenglun@yangzhenglundeMacBook-Pro.local>
2016-12-29 12:32:26 +0800

    Initial Commit

---
+++ hookPro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hookPro
+//
+//  Created by 杨政伦 on 2016/12/29.
+//  Copyright © 2016年 杨政伦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ hookPro/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  hookPro
+//
+//  Created by 杨政伦 on 2016/12/29.
+//  Copyright © 2016年 杨政伦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
